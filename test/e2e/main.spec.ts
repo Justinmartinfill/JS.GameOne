e79acf6ed01f4a20e7f1c33a349d120dbb7406c2
justinmartinfill <justinmartinfill@gmail.com>
2021-09-06 16:25:53 -0400

    we started our game!

--- test/e2e/main.spec.ts
+++ test/e2e/main.spec.ts
@@ -1,5 +1,8 @@
 describe('main', () => {
-  it('test pass', () => {
-    expect(1).to.equal(1)
+  it('has a character', () => {
+    cy.visit('http://localhost:1234')
+    cy.get('[data-test="hero"]').should('exist')
+    cy.get('[data-test="hero"]').click()
+    cy.get('[data-test="hero"]').should('have.css', 'left', '100px')
   })
 })
