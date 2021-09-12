45ac639ed9e98876d1a2c8cae7b3a77d47b2c4bf
justinmartinfill <justinmartinfill@gmail.com>
2021-09-12 16:07:37 -0400

    created directional movement and boundaries. and errors too!

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
-const hero:HTMLImageElement = document.querySelector('#hero')
+const hero = document.getElementById('hero')
 
-hero.addEventListener('click', moveRight)
+document.addEventListener('keydown', move)
 
-function moveRight () {
-  const leftPosition = hero.getBoundingClientRect().left
+/* function move (event) {
+  if (event.key === 'ArrowDown') {
+    const topPosition = hero.getBoundingClientRect().top
+    hero.style.top = topPosition + 100 + 'px'
+  }
+} */
 
-  hero.style.left = leftPosition + 100 + 'px'
+function move (event) {
+  try {
+    const Position = hero.getBoundingClientRect()
+
+    switch (event.key) {
+      case 'ArrowUp':
+        if (Position.top - 100 >= 0) {
+          hero.style.top = Position.top - 100 + 'px'
+        // move up
+        } else {
+          throw new Error('Wrong Way!')
+        }
+        break
+      case 'ArrowDown':
+        hero.style.top = Position.top + 100 + 'px'
+        // move down
+        break
+      case 'ArrowRight':
+        hero.style.left = Position.left + 100 + 'px'
+        // move right
+        break
+      case 'ArrowLeft':
+        if (Position.left - 100 >= 0) {
+          hero.style.left = Position.left - 100 + 'px'
+        // move left
+        } else {
+          throw new Error('Wrong Way!')
+        }
+        break
+    }
+  } catch (error) { alert(error) }
 }
--- test/e2e/main.spec.ts
+++ test/e2e/main.spec.ts
@@ -2,7 +2,7 @@ describe('main', () => {
   it('has a character', () => {
     cy.visit('http://localhost:1234')
     cy.get('[data-test="hero"]').should('exist')
-    cy.get('[data-test="hero"]').click()
-    cy.get('[data-test="hero"]').should('have.css', 'left', '100px')
+    cy.get('body').trigger('keypress', { keycode: 72 })
+    cy.get('[data-test="hero"]').should('have.css', 'top', '100px')
   })
 })
